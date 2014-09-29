52765aa5e424c48a805fe8bdf8aa4c65a5e5a7db
Hans Kuijpers <hans2103@gmail.com>
2014-09-29 12:31:40 +0200

    added optional analytics

--- index.php
+++ index.php
@@ -14,17 +14,13 @@ include_once JPATH_THEMES.'/'.$this->template.'/helper.php';
 <jdoc:include type="head" />
 </head>
 
-<body class="<?php echo $siteHome ; ?>-page <?php echo $option . " view-" . $view . " itemid-" . $itemid . "";?>">
+<body class="<?php echo $siteHome ; ?>-page <?php echo $option . " view-" . $view . " itemid-" . $itemid . "";?>" itemscope itemtype="http://schema.org/WebPage">
 
-    <!-- Google Tag Manager -->
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2FSQ5"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W2FSQ5');</script>
-    <!-- End Google Tag Manager -->
+<?php 
+    if ($isAnalyticsGTM || $analyticsId) { 
+        echo $analyticsScript;
+    }
+?>
 
     <header class="navbar navbar-default navbar-fixed-top mh-docs-nav" id="top" role="banner">
       <div class="container">
