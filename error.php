f51a490e3b5ce570f2642df1b4733c0126078632
Hans Kuijpers <info@hkweb.nl>
2016-08-10 11:25:02 +0200

    Update error.php

--- error.php
+++ error.php
@@ -35,17 +35,6 @@ $this->direction = $doc->direction;
 	<?php endif; ?>
 </head>
 <body>
-
-    <!-- Google Tag Manager -->
-    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-W2FSQ5"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-W2FSQ5');</script>
-    <!-- End Google Tag Manager -->
-
 	<div class="error">
 		<div id="outline">
 		<div id="errorboxoutline">
